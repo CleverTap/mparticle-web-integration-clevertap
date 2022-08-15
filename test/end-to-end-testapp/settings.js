117b376082db7a3ddd293afe7287a0258768b13c
KambleSonam <sonam@clevertap.com>
2022-08-15 20:05:02 +0530

    Added clevertap through npm

--- test/end-to-end-testapp/settings.js
+++ test/end-to-end-testapp/settings.js
@@ -1,5 +1,5 @@
 var SDKsettings = {
-    AccountID: '',
+    AccountID: '445-8W7-956Z',
     Region: "us1"
     /* fill in SDKsettings with any particular settings or options your sdk requires in order to
     initialize, this may be apiKey, projectId, primaryCustomerType, etc. These are passed
