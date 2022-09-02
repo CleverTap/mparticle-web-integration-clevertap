7d6d51e1be46cf423492ea3c544077338c3e8b1d
shwangMP <shwang@mparticle.com>
2022-09-01 21:59:17 -0700

    Implement commerce event forwarding

--- src/commerce-handler.js
+++ src/commerce-handler.js
@@ -2,65 +2,35 @@ function CommerceHandler(common) {
     this.common = common || {};
 }
 
-CommerceHandler.prototype.logCommerceEvent = function(event) {
-    /*
-        Sample ecommerce event schema:
-        {
-            CurrencyCode: 'USD',
-            DeviceId:'a80eea1c-57f5-4f84-815e-06fe971b6ef2', // MP generated
-            EventAttributes: { key1: 'value1', key2: 'value2' },
-            EventType: 16,
-            EventCategory: 10, // (This is an add product to cart event, see below for additional ecommerce EventCategories)
-            EventName: "eCommerce - AddToCart",
-            MPID: "8278431810143183490",
-            ProductAction: {
-                Affiliation: 'aff1',
-                CouponCode: 'coupon',
-                ProductActionType: 7,
-                ProductList: [
-                    {
-                        Attributes: { prodKey1: 'prodValue1', prodKey2: 'prodValue2' },
-                        Brand: 'Apple',
-                        Category: 'phones',
-                        CouponCode: 'coupon1',
-                        Name: 'iPhone',
-                        Price: '600',
-                        Quantity: 2,
-                        Sku: "SKU123",
-                        TotalAmount: 1200,
-                        Variant: '64GB'
-                    }
-                ],
-                TransactionId: "tid1",
-                ShippingAmount: 10,
-                TaxAmount: 5,
-                TotalAmount: 1215,
-            },
-            UserAttributes: { userKey1: 'userValue1', userKey2: 'userValue2' }
-            UserIdentities: [
-                {
-                    Identity: 'test@gmail.com', Type: 7
-                }
-            ]
+CommerceHandler.prototype.logCommerceEvent = function (event) {
+    if (event.EventCategory == 16 && event.ProductAction) {
+        var itemsArray = [];
+        for (var i = 0; i < event.ProductAction.ProductList.length; i++) {
+            var productDict = {}
+            for (var key in event.ProductAction.ProductList[i]) {
+                productDict[key] = event.ProductAction.ProductList[i][key];
+            }
+            itemsArray.push(productDict)
         }
-
-        If your SDK has specific ways to log different eCommerce events, see below for
-        mParticle's additional ecommerce EventCategory types:
-
-            10: ProductAddToCart, (as shown above)
-            11: ProductRemoveFromCart,
-            12: ProductCheckout,
-            13: ProductCheckoutOption,
-            14: ProductClick,
-            15: ProductViewDetail,
-            16: ProductPurchase,
-            17: ProductRefund,
-            18: PromotionView,
-            19: PromotionClick,
-            20: ProductAddToWishlist,
-            21: ProductRemoveFromWishlist,
-            22: ProductImpression
-        */
+        var chargedDict = {};
+        chargedDict['Amount'] = event.ProductAction.TotalAmount;
+        chargedDict['Charged ID'] = event.ProductAction.TransactionId;
+        chargedDict['Items'] = itemsArray;
+        clevertap.event.push("Charged", chargedDict)
+    } else {
+        var listOfPageEvents = mParticle.eCommerce.expandCommerceEvent(
+            event
+        );
+        if (listOfPageEvents != null) {
+            for (var i = 0; i < listOfPageEvents.length; i++) {
+                if (listOfPageEvents[i].EventAttributes == null) {
+                    clevertap.event.push(listOfPageEvents[i].EventName)
+                } else {
+                    clevertap.event.push(listOfPageEvents[i].EventName, listOfPageEvents[i].EventAttributes);
+                }
+            }
+        }
+    }
 };
 
 module.exports = CommerceHandler;
