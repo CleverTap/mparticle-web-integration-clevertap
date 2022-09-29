b02df0ffc25a41e83c5d2652212154cbf238ec16
shwangMP <shwang@mparticle.com>
2022-09-29 14:39:58 -0500

    Test S2S forwarder setting

--- test/end-to-end-testapp/settings.js
+++ test/end-to-end-testapp/settings.js
@@ -1,6 +1,7 @@
 var SDKsettings = {
     accountID: '445-8W7-956Z',
-    region: "us1"
+    region: 'us1',
+    forwardWebRequestsServerSide: 'true'
     /* fill in SDKsettings with any particular settings or options your sdk requires in order to
     initialize, this may be apiKey, projectId, primaryCustomerType, etc. These are passed
     into the src/initialization.js file as the
